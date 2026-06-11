5aa73c3ff98c52ff5476b415e6bd1cdf9086255f
d3bvstack <dbarba-v@student.42madrid.com>
2026-06-11 16:04:32 +0200

    feat: add SIGQUIT as signal to listen for to change keep_running state

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dbarba-v <dbarba-v@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/10 18:20:17 by dbarba-v          #+#    #+#             */
-/*   Updated: 2026/06/10 23:07:33 by dbarba-v         ###   ########.fr       */
+/*   Updated: 2026/06/11 15:35:20 by dbarba-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@
 
 volatile sig_atomic_t keep_running = 1;
 
-void stop(int) 
-{ 
-    keep_running = 0; 
+void stop(int)
+{
+    keep_running = 0;
 }
-
 
 int main(int argc, char** argv)
 {
     std::signal(SIGINT, stop);
+    std::signal(SIGQUIT, stop);
     std::signal(SIGTERM, stop);
-    
+
     (void)argc;
     (void)argv;
     while (keep_running)
