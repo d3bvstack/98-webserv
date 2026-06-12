05fe199d50d91240ec70072af7cdf0233798d85e
d3bvstack <dbarba-v@student.42madrid.com>
2026-06-12 10:50:37 +0200

    docs: add Doxygen comments to Server class functions

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,12 +6,20 @@
 /*   By: dbarba-v <dbarba-v@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 22:00:54 by dbarba-v          #+#    #+#             */
-/*   Updated: 2026/06/12 00:15:50 by dbarba-v         ###   ########.fr       */
+/*   Updated: 2026/06/12 10:50:23 by dbarba-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
+/**
+ * @brief Checks if the string `filename` ends with the provided `extension`
+ *
+ * @param filename String to search on
+ * @param extension String to search for
+ * @return `true` or
+ * @return `false`
+ */
 static bool matchExtension(const std::string& filename, const std::string extension)
 {
     bool extensionMatch = false;
@@ -21,16 +29,20 @@ static bool matchExtension(const std::string& filename, const std::string extens
         std::string endOfString = filename.substr(filename.length() - extension.length());
         extensionMatch = (endOfString == extension);
     }
-    
+
     return (extensionMatch);
 }
 
+/**
+ * @brief Construct a new Server:: Server object retrieving config files from default location
+ *
+ */
 Server::Server()
 {
     DIR* dirStream = opendir(DEFAULT_CONFIG_DIR);
     if (dirStream == NULL)
         throw std::runtime_error("[ERROR] Could not open default config directory " DEFAULT_CONFIG_DIR ".");
-    
+
     struct dirent* dirEntry;
     while ((dirEntry = readdir(dirStream)) != NULL)
     {
@@ -48,6 +60,13 @@ Server::Server()
         throw std::runtime_error("[ERROR] No valid configuration files where provided.");
 }
 
+
+/**
+ * @brief Construct a new Server:: Server object using the arguments as filepaths to configuration files
+ *
+ * @param argc Number of arguments
+ * @param argv Array of arguments
+ */
 Server::Server(int argc, char **argv)
 {
     for (int i = 1; i < argc; ++i)
@@ -63,6 +82,10 @@ Server::Server(int argc, char **argv)
         throw std::runtime_error("[ERROR] No valid configuration files where provided.");
 }
 
+/**
+ * @brief Prints the Server object members for debugging
+ *
+ */
 void Server::debugServer()
 {
     int i = 0;
